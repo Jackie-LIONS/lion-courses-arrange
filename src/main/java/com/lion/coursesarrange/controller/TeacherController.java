3238452149f377cda32c201e941c94e7ecb9ab7d
Jackie <2373489842@qq.com>
2024-05-28 09:22:47 +0800

    "教师功能修改"

--- src/main/java/com/lion/coursesarrange/controller/TeacherController.java
+++ src/main/java/com/lion/coursesarrange/controller/TeacherController.java
@@ -2,29 +2,24 @@ package com.lion.coursesarrange.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lion.coursesarrange.model.dto.teacher.TeacherLoginRequest;
-import com.lion.coursesarrange.model.dto.user.UserLoginRequest;
 import com.lion.coursesarrange.model.enums.CodeEnum;
 import com.lion.coursesarrange.model.pojo.Teacher;
-import com.lion.coursesarrange.model.pojo.User;
 import com.lion.coursesarrange.model.result.BaseResult;
 import com.lion.coursesarrange.model.result.BusException;
 import com.lion.coursesarrange.service.TeacherService;
-import com.lion.coursesarrange.utils.CheckCodeUtil;
 import com.lion.coursesarrange.utils.JWTUtil;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
+
     @PostMapping("/login")
     public BaseResult<String> login(@RequestBody TeacherLoginRequest teacherLoginRequest, HttpServletRequest request){
         // 获取输入的验证码
@@ -37,28 +32,7 @@ public class TeacherController {
         String token = teacherService.login(teacherLoginRequest.getTeacherName(), teacherLoginRequest.getPassword());
         return BaseResult.ok(token);
     }
-
-    /**
-     * 验证码生成
-     * @param request
-     * @param response
-     * @throws IOException
-     */
-    @GetMapping("/checkCode")
-    public void checkCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        //生成验证码并返回
-        String checkCodeGen = CheckCodeUtil.generateVerifyCode(4);
-
-        //存入session
-        HttpSession session = request.getSession();
-        session.setAttribute("checkCodeGen",checkCodeGen);
-
-        System.out.println(session.getId());
-        //输出图片
-        ServletOutputStream os = response.getOutputStream();
-        CheckCodeUtil.outputImage(100,40,os,checkCodeGen);
-    }
+//    }
 
     @PostMapping("/register")
     public BaseResult register(@RequestBody Teacher teacher){
